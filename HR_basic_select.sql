38cb34fc2bddcbbd6e8cb2b6a7facd8009152c61
Steve Kane <quantumkane00@gmail.com>
2020-12-31 10:00:55 -0600

    through #39

--- HR_basic_select.sql
+++ HR_basic_select.sql
@@ -274,3 +274,18 @@ SELECT CONCAT('There are a total of ',
 FROM occupations 
 GROUP BY Occupation 
 ORDER BY COUNT(Occupation); 
+
+# 39. Write a query to print the company_code, founder name, total number of lead managers, total number of senior managers, total number of managers, and total number of employees.
+
+SELECT c.company_code, c.founder, 
+    COUNT(distinct l.lead_manager_code), 
+    COUNT(distinct s.senior_manager_code), 
+    COUNT(distinct m.manager_code),
+    COUNT(distinct e.employee_code) 
+FROM Company c, Lead_Manager l, Senior_Manager s, Manager m, Employee e 
+WHERE c.company_code = l.company_code 
+    AND l.lead_manager_code = s.lead_manager_code 
+    AND s.senior_manager_code = m.senior_manager_code 
+    AND m.manager_code = e.manager_code 
+GROUP BY c.company_code, c.founder 
+ORDER BY c.company_code;
