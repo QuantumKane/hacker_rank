a41d47e4c272190e7871a931bdd004cb35f23107
Steve Kane <quantumkane00@gmail.com>
2021-01-02 16:05:56 -0600

    through #42

--- HR_basic_select.sql
+++ HR_basic_select.sql
@@ -304,3 +304,25 @@ ORDER BY N;
 
 # 41. ot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation.
 
+SET @r1=0, @r2=0, @r3=0, @r4=0;
+SELECT MIN(Doctor), MIN(Professor), MIN(Singer), MIN(Actor)
+FROM(
+  SELECT CASE WHEN Occupation = 'Doctor' then (@r1:=@r1+1)
+            WHEN Occupation = 'Professor' then (@r2:=@r2+1)
+            WHEN Occupation = 'Singer' then (@r3:=@r3+1)
+            WHEN Occupation = 'Actor' then (@r4:=@r4+1) end as RowNumber,
+    CASE WHEN Occupation = 'Doctor' then Name end as Doctor,
+    CASE WHEN Occupation = 'Professor' then Name end as Professor,
+    CASE WHEN Occupation = 'Singer' then Name end as Singer,
+    CASE WHEN Occupation = 'Actor' then Name end as Actor
+  FROM OCCUPATIONS
+  ORDER BY Name
+) Temp
+GROUP BY RowNumber
+
+# 42. 
+
+SELECT IF(GRADE < 8, NULL, NAME), GRADE, MARKS
+FROM STUDENTS JOIN GRADES
+WHERE MARKS BETWEEN MIN_MARK AND MAX_MARK
+ORDER BY GRADE DESC, NAME
